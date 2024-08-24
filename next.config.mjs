013905640d72cbd3e6d052e167b612d4a4c8cbd8
BriLeighk <kirchgessner@wisc.edu>
2024-08-24 18:16:50 -0400

    made header component; added image domain patterns in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ],
+        domains: ['www.techsmith.com'],
+    },
+};
 
 export default nextConfig;
