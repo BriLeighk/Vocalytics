9da93cf75d624cbd0fbf93c002c26e5917490b13
BriLeighk <kirchgessner@wisc.edu>
2024-08-24 11:48:17 -0400

    first commit; project structure

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Vocalytics",
+  description: "Your one stop shop for all your transcription needs.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
