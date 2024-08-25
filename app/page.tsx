7c8a84d8e665aad50e2cd143dc3f09b9027c6cec
BriLeighk <kirchgessner@wisc.edu>
2024-08-25 00:49:48 -0400

    re-fixing next.js issues without aos

--- app/page.tsx
+++ app/page.tsx
@@ -1,111 +1,12 @@
 'use client'
-import { useState, useEffect } from 'react'
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import AOS from "aos";
-import "aos/dist/aos.css";
+import Header from './Components/header'
+import Image from 'next/image'
 
-const navigation = [
-  { name: 'Sign Up', href: '#' },
-  { name: 'Transcript Viewer', href: '#' },
-]
-
-function App() {
-  useEffect(() => {
-    AOS.init();
-    AOS.refresh();
-  }, []);
-
-  // Rest of your App component
-}
 
 export default function Home() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
   return (
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Vocalytics</span>
-              <img
-                alt=""
-                src="" /* IF TIME CREATE LOGO */
-                className="h-8 w-auto"
-              />
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="h-6 w-6" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
-                {item.name}
-              </a>
-            ))}
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div>
-        </nav>
-        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-          <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Vocalytics</span>
-                <img
-                  alt=""
-                  src="" /* IF TIME CREATE LOGO */
-                  className="h-8 w-auto"
-                />
-              </a>
-              <button
-                type="button"
-                onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon aria-hidden="true" className="h-6 w-6" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-                <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Log in
-                  </a>
-                </div>
-              </div>
-            </div>
-          </DialogPanel>
-        </Dialog>
-      </header>
+      <Header />
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
@@ -121,8 +22,7 @@ export default function Home() {
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-          </div>
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
               Vocalytics
@@ -148,34 +48,16 @@ export default function Home() {
             <br></br>
             {/* temp solution - to be replaced */}
             <p className="text-gray-600">
-              Effortlessly transcribe your mp3 or mp4 files, highlight key moments, and generate insightful summaries. With Vocalytics, you'll unlock the full potential of your audio and video content. Our intuitive interface and powerful AI technology make it a breeze to analyze, understand, and share your valuable recordings.
+              Effortlessly transcribe your mp3 or mp4 files, highlight key moments, and generate insightful summaries. With Vocalytics, you&apos;ll unlock the full potential of your audio and video content. Our intuitive interface and powerful AI technology make it a breeze to analyze, understand, and share your valuable recordings.
             </p>
           </div>
           <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/2">
-            <img
+            <Image
               src="https://www.techsmith.com/blog/wp-content/uploads/2023/08/voice-over.png"
               alt="Description of your image"
               className="w-full h-auto rounded-lg shadow-md"
-            />
-          </div>
-        </div>
-
-        <div className="flex items-center justify-between max-w-3xl mx-auto p-4">
-          <div className="flex-1 pr-8">
-            <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Your Audio, Insights, Made Easy.
-            </h2>
-            <br></br>
-            {/* temp solution - to be replaced */}
-            <p className="text-gray-600">
-              Effortlessly transcribe your mp3 or mp4 files, highlight key moments, and generate insightful summaries. With Vocalytics, you'll unlock the full potential of your audio and video content. Our intuitive interface and powerful AI technology make it a breeze to analyze, understand, and share your valuable recordings.
-            </p>
-          </div>
-          <div data-aos="fade-right" className="w-full sm:w-1/2 md:w-1/3 lg:w-1/2">
-            <img
-              src="https://www.techsmith.com/blog/wp-content/uploads/2023/08/voice-over.png"
-              alt="Description of your image"
-              className="w-full h-auto rounded-lg shadow-md"
+              width={500}
+              height={300}
             />
           </div>
         </div>
@@ -199,17 +81,14 @@ export default function Home() {
             </p>
           </div>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Try Vocalytics for free today ! 
-              </a>
-            </div>
-          
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Try Vocalytics for free today !
+            </a>
+          </div>
         </div>
-
-
 
         <div
           aria-hidden="true"
